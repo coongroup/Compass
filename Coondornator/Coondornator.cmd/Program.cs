6eed364e2e541e13dd6445654c829c0d23bf1fe5
Nicholas Kwiecien <kwiecien@wisc.edu>
2013-04-08 16:16:43 -0500

    F

--- Coondornator/Coondornator.cmd/Program.cs
+++ Coondornator/Coondornator.cmd/Program.cs
@@ -11,7 +11,7 @@ namespace Compass.Coondornator
         static void Main(string[] args)
         {
             CondorSubmitFile submitFile = new CondorSubmitFile();
-            Console.WriteLine(submitFile.SubmitFileHeader("hi nick", "hi nick"));
+            Console.WriteLine(submitFile.SubmitFileHeader("executable", "requirements"));
             submitFile.Requirements.Add("Derek Rocks");
             submitFile.Requirements.Add("Biking is fun");
 

eca0a14178b5ed41b6d3a07c1d63f83c7e75faf5
Derek Bailey <dbaileychess@gmail.com>
2013-09-04 17:04:18 -0500

    added new TagQuant

---
+++ TagQuant/QuantFile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LumenWorks.Framework.IO.Csv;
+
+namespace TagQuant
+{
+    public class QuantFile
+    {
+        public Dictionary<int, PSM> Psms; 
+
+        public string FilePath { get; set; }
+
+        public QuantFile(string filePath)
+        {
+            FilePath = filePath;
+            Psms = new Dictionary<int, PSM>();
+        }
+
+        public void AddPSM(PSM psm)
+        {
+           Psms.Add(psm.SpectrumNumber, psm);
+        }
+        
+        public PSM this[int scanNumber]
+        {
+            get
+            {
+                return Psms[scanNumber];
+            }
+        }
+    }
+}
---
+++ TagQuant/TagSetType.cs
@@ -0,0 +1,9 @@
+﻿namespace TagQuant
+{
+    public enum TagSetType
+    {
+        TMTC = 0,
+        TMTN = 1,
+        iTRAQ = 2
+    }
+}

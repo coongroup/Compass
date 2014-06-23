e2e7c38546cfa1d778a83b4a5419b1ac9a432c8a
Derek Bailey <dbaileychess@gmail.com>
2014-06-23 13:24:24 -0500

    Updated build script

--- release.bat
+++ release.bat
@@ -1,4 +1,6 @@
 @ECHO OFF
+@ECHO Building Compass...
+
 SET bindir="bin\Release"
 SET objdir="COMPASS\obj"
 SET devenv="C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe"
@@ -10,14 +12,12 @@ rmdir /s /q %objdir%
 
 mkdir %bindir%
 
-@ECHO ON
-
 %devenv% "COMPASS\COMPASS.sln" /rebuild "Release" /project "COMPASS"
 %devenv% "COMPASS\COMPASS.sln" /rebuild "Release|x86" /project "COMPASS"
 %devenv% "COMPASS\COMPASS.sln" /rebuild "Release|x64" /project "COMPASS"
 
-%zip% a -tzip "%bindir%\COMPASS-%version% (32-bit).zip" ".\%bindir%\32-bit\*"
-%zip% a -tzip "%bindir%\COMPASS-%version% (64-bit).zip" ".\%bindir%\64-bit\*"
-%zip% a -tzip "%bindir%\COMPASS-%version%.zip" ".\%bindir%\Any Cpu\*"
+%zip% a -tzip "%bindir%\COMPASS-%version% (32-bit).zip" ".\%bindir%\32-bit\*" > NUL
+%zip% a -tzip "%bindir%\COMPASS-%version% (64-bit).zip" ".\%bindir%\64-bit\*" > NUL
+%zip% a -tzip "%bindir%\COMPASS-%version%.zip" ".\%bindir%\Any Cpu\*" > NUL
 
 PAUSE
